002f481b58d0024b7acde95dda2280979f61db4f
geet <geet@glasierinc.com>
2019-02-26 17:16:11 +0530

    ex-4

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -63,8 +63,30 @@ Theta2_grad = zeros(size(Theta2));
 %
 
 
+X = [ones(m, 1) X];
+Z2 = (Theta1 * X')';
+A2 = sigmoid(Z2);
+A2 = [ones(size(A2,1), 1) A2];
+Z3 = (Theta2 * A2')';
+H = sigmoid(Z3);
+y_matrix = eye(num_labels)(y,:);
 
+J = sum(sum(-y_matrix .* log(H) - (1 - y_matrix) .* log(1 - H))) ./ m;
+J_Reg = (sum(sum(Theta1(:,[2:end]) .^ 2)) + sum(sum(Theta2(:,[2:end]) .^ 2))) * (lambda/(2*m));
+J = J + J_Reg;
 
+delta3 = H - y_matrix;
+delta2 = (delta3 * Theta2(:,[2:end])) .* sigmoidGradient(Z2);
+
+Theta1_grad = delta2 ./ m;
+Theta2_grad = delta3 ./ m;
+
+% size(delta2)
+size(delta3)
+size(Theta2(:,[2:end]))
+% size(Z2)
+size(Theta1)
+size(Theta2)
 
 
 
